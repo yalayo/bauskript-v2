fda170a32c9cb0b32319e4393cc068e1ef0c2d97
Yunior Alayo <yunior.alayo@gmail.com>
2025-04-24 22:32:04 +0200

    Updated to wait for the "survey" dom element to be rendered.

--- bases/frontend/src/app/frontend/survey/main.cljs
+++ bases/frontend/src/app/frontend/survey/main.cljs
@@ -4,11 +4,15 @@
             [app.frontend.survey.events :as events]
             [app.frontend.survey.views :as views]))
 
-(defn ^:dev/after-load mount-root []
-  (re-frame/clear-subscription-cache!)
+(defn mount-root []
   (let [root-el (.getElementById js/document "survey")]
-    (rdom/unmount-component-at-node root-el)
-    (rdom/render [views/app] root-el)))
+    (if root-el
+      (do
+        (re-frame/clear-subscription-cache!)
+        (rdom/unmount-component-at-node root-el)
+        (rdom/render [views/app] root-el))
+      ;; If #survey doesn't exist yet, try again in 100ms
+      (js/setTimeout mount-root 100))))
 
 (defn init []
   (re-frame/dispatch-sync [::events/initialize-db])
